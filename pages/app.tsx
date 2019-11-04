c329643103b1336f3787825ecc6704937cb3f118
Asher Foster <me@asherfoster.com>
2019-11-05 12:50:28 +1300

    Configure prod deployment

---
+++ pages/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AppRedirectPage = () => {
+  window.location.href = 'https://app.canal.nz';
+  return (
+    <h1>Redirecting you...</h1>
+  );
+};
+
+export default AppRedirectPage;
